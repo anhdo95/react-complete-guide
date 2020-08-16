4d6062511a09a45681519aa4df2788ec94794d88
Anh Do <dinhanhdo95@gmail.com>
2020-08-16 18:01:15 +0700

    Use react-transition-group to animate the Modal component instead

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Transition } from 'react-transition-group'
 
 import classes from './Modal.module'
 
 function Modal(props) {
 	return (
-		<>
-			{props.show && <div className={classes.Backdrop} onClick={props.onBackdropClick}></div>}
-			<div className={[classes.Modal, props.show && classes.ModalOpen].join(' ')}>
-				{props.children}
-			</div>
-		</>
+		<Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+			{state => (
+				<div className={classes.Modal}>
+					{<div className={classes.Backdrop} onClick={props.onBackdropClick}></div>}
+					<div className={[classes.ModalContent, classes[state]].join(' ')}>
+						{props.children}
+					</div>
+				</div>
+			)}
+		</Transition>
 	)
 }
 
